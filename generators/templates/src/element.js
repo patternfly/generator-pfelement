52271af1346459cc2c287b14e7373b459f0119e1
castastrophe <casnroberts+github@gmail.com>
2019-01-30 14:18:21 -0500

    Additional template updates, references, and clean-up

--- generators/templates/src/element.js
+++ generators/templates/src/element.js
@@ -5,12 +5,12 @@ class <%= elementClassName %> extends PFElement {
     return "<%= elementName %>";
   }
 
-  get templateUrl() {
-    return "<%= elementName %>.html";
-  }
-
   get schemaUrl() {
     return "<%= elementName %>.json";
+  }
+
+  get templateUrl() {
+    return "<%= elementName %>.html";
   }
 
   get styleUrl() {
@@ -31,19 +31,36 @@ class <%= elementClassName %> extends PFElement {
   // }
 <%_ } _%>
 
+  // Declare the type of this component
+  static get PfeType() {
+    return PFElement.PfeTypes.<%= _capitalize(template_type) %>;
+  }
+
   constructor() {
-    super(<%= elementClassName %>);
+    super(<%= elementClassName %>, { type: PfeBand.PfeType });
   }
 
   // connectedCallback() {
   //   super.connectedCallback();
+  //   // If you need to initialize any attributes, do that here
   // }
 
   // disconnectedCallback() {}
 
 <%_ if (attributes.length > 0) { _%>
   // Process the attribute change
-  attributeChangedCallback(attr, oldValue, newValue) {}
+  attributeChangedCallback(attr, oldValue, newValue) {
+    super.attributeChangedCallback(attr, oldValue, newValue);
+    // Strip the prefix from the attribute
+    attr = attr.replace("pfe-", "");
+    // If the observer is defined in the attribute properties
+    if (this[attr] && this[attr].observer) {
+      // Get the observer function
+      let observer = this[this[attr].observer].bind(this);
+      // If it's a function, allow it to run
+      if (typeof observer === "function") observer(attr, oldValue, newValue);
+    }
+  }
 <%_ } else { _%>
   // attributeChangedCallback(attr, oldValue, newValue) {}
 <%_ } _%>
@@ -51,4 +68,4 @@ class <%= elementClassName %> extends PFElement {
 
 PFElement.create(<%= elementClassName %>);
 
-export default <%= elementClassName %>;
+export { <%= elementClassName %> as default };
--- generators/templates/src/element.story.js
+++ generators/templates/src/element.story.js
@@ -1,47 +1,49 @@
 import { storiesOf } from "@storybook/polymer";
-import { withKnobs, text, select } from "@storybook/addon-knobs/polymer";
-import "./<%= elementName %>";
+import * as bridge from "@storybook/addon-knobs/polymer";
+import * as tools from "../../../.storybook/utils.js";
+
+import <%= elementClassName %> from "../<%= elementName %>";
 
 const stories = storiesOf("<%= readmeName %>", module);
-stories.addDecorator(withKnobs);
 
-stories.add("<%= elementName %>", () => {
-  // Attributes
-  <% for(let i = 0; i < attributes.length; i++) { %>
-  const <% attributes[i] %>Label = <% _.capitalize(attributes[i]) %>;
-  const <% attributes[i] %>Options = {
-    default: "default"
-  };
-  const <% attributes[i] %>Default = "default";
-  const <% attributes[i] %>Value = select(<% attributes[i] %>Label, <% attributes[i] %>Options, <% attributes[i] %>Default);
-  let <% attributes[i] %>Attr = <% attributes[i] %>Value != "default" ? ` <% attributes[i] %>="${<% attributes[i] %>Value}"` : "";
-  <% } %>
+// Define the template to be used
+const template = (data = {}) => {
+  return tools.component(<%= elementClassName %>.tag, data.prop, data.slots);
+};
 
-  // Slots
-  <% for(let i = 0; i < slots.length; i++) { %>
-    const <% slots[i] %>Label = "<% _.capitalize(slots[i]) %>";
-    const <% slots[i] %>Default = "Lorem ipsum";
-    let <% slots[i] %>Value = text(<% slots[i] %>Label, <% slots[i] %>Default);
-    <% } %>
+// Use these to get dynamically generated content
+// const defaultHeading = tools.autoHeading(true);
+const defaultContent = tools.autoContent(1, 2);
 
-  return `
-  <section>
-    <h2>Your RH Element</h2>
-    <<%= elementName %><% for(let i = 0; i < attributes.length; i++) { %>${<% attributes[i] %>Attr} <% } %>>
-      <%- for(let i = 0; i < slots.length; i++) { %>
-        ${<% slots[i] %>Value ? '<span slot="<% slots[i] %>">' + <% slots[i] %>Value + '</span>' : ''}
-      <% } -%>
-    </<%= elementName %>>
-  </section>
-  <section>
-    <h2>Markup</h2>
-    <pre><code>
-    &lt;<%= elementName %><% for(let i = 0; i < attributes.length; i++) { %>${<% attributes[i] %>Attr} <% } %>&gt;
-    <%- for(let i = 0; i < slots.length; i++) { %>
-      ${<% slots[i] %>Value ? '&lt;div slot="<% slots[i] %>"&gt;\n\t' + escapeHTML(<% slots[i] %>Value) + '\n      &lt;/div&gt;' : ''}
-    <% } -%>
-    &lt;/<%= elementName %>&gt;
-    </code></pre>
-  </section>
-  `
+stories.addDecorator(bridge.withKnobs);
+
+stories.add(<%= elementClassName %>.tag, () => {
+  let config = {};
+  const props = <%= elementClassName %>.properties;
+
+  //-- Set any custom defaults just for storybook here
+
+  // Trigger the auto generation of the knobs for attributes
+  config.prop = tools.autoPropKnobs(props, bridge);
+
+  const slots = <%= elementClassName %>.slots;
+
+  //-- Set any custom content for the slots here
+  
+  // Trigger the auto generation of the knobs for slots
+  config.has = tools.autoContentKnobs(slots, bridge);
+
+  //-- Build your slots here using config.has["slot-name"] to get user content
+  // prettier-ignore
+  config.slots = [{
+    content: defaultContent
+  }];
+
+  //-- Reset default values show they don't render in the markup
+  // if (config.prop["prop-name"] === "default") {
+  //   config.prop["prop-name"] = "";
+  // }
+
+  const rendered = template(config);
+  return styles + tools.preview(rendered);
 });
