58a7a01caee10de06e9191a41e466f22c1210869
mwcz <mwc@clayto.com>
2019-05-29 16:06:36 -0400

    move the story file to the demo dir

--- generators/index.js
+++ generators/index.js
@@ -243,11 +243,11 @@ module.exports = class extends Generator {
     }
 
     if (isPfelement) {
-      if (fs.existsSync(this.templatePath("src/element.story.js"))) {
+      if (fs.existsSync(this.templatePath("demo/element.story.js"))) {
         this.fs.copyTpl(
-          this.templatePath("src/element.story.js"),
+          this.templatePath("demo/element.story.js"),
           this.destinationPath(
-            `${this.props.elementName}/src/${this.props.elementName}.story.js`
+            `${this.props.elementName}/demo/${this.props.elementName}.story.js`
           ),
           this.props
         );
