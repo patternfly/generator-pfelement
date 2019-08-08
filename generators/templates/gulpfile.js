25932bd247b5e2143e9a466158f576702f72ee62
Kyle Buchanan <kylebuch8@gmail.com>
2019-08-08 10:53:03 -0400

    passing the entire package.json object to the gulp factory
    In the gulp factory, we're using more than just the pfelement property so it makes sense to just send the whole package.json file so we have access to all of the properties.

--- generators/templates/gulpfile.js
+++ generators/templates/gulpfile.js
@@ -1,4 +1,4 @@
 const gulpFactory = require("<%= gulpFactoryLocation %>");
 const pfelementPackage = require("./package.json");
 
-gulpFactory(pfelementPackage.pfelement);
+gulpFactory(pfelementPackage);
